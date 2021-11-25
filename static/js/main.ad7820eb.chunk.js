4c7a1d368c3f456c6545aba3244d3238d79d65c0
Serhii Kondratiuk <sergey932015@gmail.com>
2021-11-25 16:08:08 +0200

    Updates

---
+++ static/js/main.ad7820eb.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],{11:function(t,n,e){"use strict";e.r(n);var c=e(0),o=e.n(c),r=e(3),i=e.n(r);e(8),e(9);var s=function(){return 5},a=e(2);i.a.render(Object(a.jsx)(o.a.StrictMode,{children:Object(a.jsx)(s,{})}),document.getElementById("root"))},8:function(t,n,e){},9:function(t,n,e){}},[[11,1,2]]]);
+//# sourceMappingURL=main.ad7820eb.chunk.js.map
